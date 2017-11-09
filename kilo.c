a1808854db9b29175280296a0c317dd82d9864bb
mastize <mastizenone@gmail.com>
2017-11-09 12:33:52 -0800

    step 155

--- kilo.c
+++ kilo.c
@@ -41,7 +41,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 /*** data ***/
@@ -196,25 +197,38 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** syntax highlighting ***/
 
+int is_separator(int c) {
+    return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
 void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    int i;
-    for (i = 0; i < row->rsize; i++) {
-        if (isdigit(row->render[i])) {
-            row->hl[i] = HL_NUMBER;
-        }
-    }
+    int prev_sep = 1;
 
+    int i;
+    while (i < row->rsize) {
+        char c = row->render[i];
+        unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) {
+            row->hl[i] = HL_NUMBER;
+            i++;
+            prev_sep = 0;
+            continue;
+        }
+
+        prev_sep = is_separator(c);
+        i++;
+    }
 }
 
 int editorSyntaxToColor(int hl) {
     switch (hl) {
-    case HL_NUMBER:
-        return 31;
-    default:
-        return 37;
+    case HL_NUMBER: return 31;
+    case HL_MATCH: return 34;
+    default: return 37;
     }
 }
 
@@ -460,6 +474,15 @@ void editorFindCallback(char *query, int key) {
     static int last_match = -1;
     static int direction = 1;
 
+    static int saved_hl_line;
+    static char *saved_hl = NULL;
+
+    if (saved_hl) {
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
     if (key == '\r' || key == '\x1b') {
         last_match = -1;
         direction = 1;
@@ -489,6 +512,11 @@ void editorFindCallback(char *query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
@@ -582,17 +610,24 @@ void editorDrawRows(struct abuf *ab) {
             if (len > E.screencols) len = E.screencols;
             char *c = &E.row[filerow].render[E.coloff];
             unsigned char *hl = &E.row[filerow].hl[E.coloff];
+            int current_color = -1;
             int j;
 
             for (j = 0; j < len; j++) {
                 if (hl[j] == HL_NORMAL) {
-                    abAppend(ab, "\x1b[39m", 5);
+                    if (current_color != -1) {
+                        abAppend(ab, "\x1b[39m", 5);
+                        current_color = -1;
+                    }
                     abAppend(ab, &c[j], 1);
                 } else {
                     int color = editorSyntaxToColor(hl[j]);
-                    char buf[16];
-                    int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
-                    abAppend(ab, buf, clen);
+                    if (color != current_color) {
+                        current_color = color;
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+                        abAppend(ab, buf, clen);
+                    }
                     abAppend(ab, &c[j], 1);
                 }
             }
